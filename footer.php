a949b57fb21f825c180a9527665d59454b8329c7
SirajMunir2018 <41114231+SirajMunir2018@users.noreply.github.com>
2024-10-02 16:56:10 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,256 @@
+<?php
+function footer_logo()
+{
+    ?>
+    <div class="image"><img decoding="async" src="wp-content/uploads/new_pics/Logo.png" alt="Al-balatin-logo-2-05w" /></div>
+    <?php
+}
+
+function footer_about()
+{
+    ?>
+    <div class="heading">Welcome to Abdullah Saeed Al-Hunaidi Contracting Co., a leading provider
+        of manpower supply and industrial support services in the
+        Kingdom of Saudi Arabia.</div>
+    <?php
+}
+function footer_scripts_whatsapp()
+{
+    ?>	
+	<script type="text/html" id="wpb-modifications"> window.wpbCustomElement = 1; </script>
+	<link rel='stylesheet' id='vc_animate-css-css'
+		href='wp-content/plugins/js_composer/assets/lib/bower/animate-css/animate.min555b.css?ver=6.13.0'
+		type='text/css' media='all' />
+	<link rel='stylesheet' id='whatsapp-widget-css-css' href='wp-content/plugins/whatsapp-chat/style380f.html?ver=6.5.4'
+		type='text/css' media='all' />
+	<script type="text/javascript" src="wp-content/themes/healsoul/assets/libs/picturefill/picturefill.min.js"
+		id="picturefill-js"></script>
+	<script type="text/javascript"
+		src="wp-content/themes/healsoul/assets/libs/mousewheel/jquery.mousewheel1a7b.js?ver=1.7.7"
+		id="mousewheel-js"></script>
+	<script type="text/javascript" src="wp-content/themes/healsoul/assets/libs/light-gallery/js/lightgallery-all.js"
+		id="lightgallery-js"></script>
+	<script type="text/javascript"
+		src="wp-content/themes/healsoul/assets/libs/smooth-scroll-for-web/SmoothScroll.min5f04.js?ver=1.4.6"
+		id="smooth-scroll-js"></script>
+	<script type="text/javascript"
+		src="wp-content/themes/healsoul/assets/libs/smooth-scroll/jquery.smooth-scroll.min1a7b.js?ver=1.7.7"
+		id="jquery-smooth-scroll-js"></script>
+	<script type="text/javascript" src="wp-content/themes/healsoul/assets/libs/swiper/js/swiperfa0c.js?ver=4.0.3"
+		id="swiper-js"></script>
+	<script type="text/javascript"
+		src="wp-content/themes/healsoul/assets/libs/matchheight/jquery.matchheight-min1a7b.js?ver=1.7.7"
+		id="matchheight-js"></script>
+	<script type="text/javascript"
+		src="wp-content/plugins/js_composer/assets/lib/vc_waypoints/vc-waypoints.min555b.js?ver=6.13.0"
+		id="vc_waypoints-js"></script>
+	<script type="text/javascript"
+		src="wp-content/themes/healsoul/assets/libs/smartmenus/jquery.smartmenus1a7b.js?ver=1.7.7"
+		id="smartmenus-js"></script>
+	<script type="text/javascript"
+		src="wp-content/plugins/js_composer/assets/js/dist/js_composer_front.min555b.js?ver=6.13.0"
+		id="wpb_composer_front_js-js"></script>
+	<script type="text/javascript" id="healsoul-script-js-extra">
+		/* <![CDATA[ */
+		var $insight = { "isRTL": "", "templateUrl": "https:\/\/www.albalatin.com.sa\/wp-content\/themes\/healsoul", "ajaxurl": "https:\/\/www.albalatin.com.sa\/wp-admin\/admin-ajax.php", "primary_color": "#cca000", "header_sticky_enable": "", "header_sticky_height": "77", "scroll_top_enable": "1", "lazyLoadImages": "", "light_gallery_auto_play": "0", "light_gallery_download": "1", "light_gallery_full_screen": "1", "light_gallery_zoom": "1", "light_gallery_thumbnail": "1", "light_gallery_share": "1", "mobile_menu_breakpoint": "1199", "isSingleProduct": "", "like": "Like", "unlike": "Unlike" };
+		/* ]]> */
+	</script>
+	<script type="text/javascript" src="wp-content/themes/healsoul/assets/js/main1a7b.js?ver=1.7.7"
+		id="healsoul-script-js"></script>
+	<script type="text/javascript" id="insight-view-js-extra">
+		/* <![CDATA[ */
+		var ic_vars = { "ajax_url": "https:\/\/www.albalatin.com.sa\/wp-admin\/admin-ajax.php", "ic_nonce": "a83e4a31c3", "post_id": "2419" };
+		/* ]]> */
+	</script>
+	<script type="text/javascript" src="wp-content/plugins/insight-core/assets/js/insight-view1a7b.js?ver=1.7.7"
+		id="insight-view-js"></script>
+    <?php
+}
+function footer_links()
+{
+    ?>
+    <div class="menu-footer-useful-links-container">
+        <ul id="menu-footer-useful-links" class="menu">
+            <li id="menu-item-3490"
+                class="menu-item menu-item-type-post_type menu-item-object-service menu-item-3490 level-1">
+                <a href="https://accksa.com/service/manpower-supply/">
+                    <div class="menu-item-wrap">
+                        <span class="menu-item-title">Manpower
+                            Supply</span>
+                    </div>
+                </a>
+            </li>
+            <li id="menu-item-3491"
+                class="menu-item menu-item-type-post_type menu-item-object-service menu-item-3491 level-1">
+                <a href="https://accksa.com/service/material-supply">
+                    <div class="menu-item-wrap">
+                        <span class="menu-item-title">Material
+                            Supply</span>
+                    </div>
+                </a>
+            </li>
+            <li id="menu-item-3878"
+                class="menu-item menu-item-type-post_type menu-item-object-service menu-item-3878 level-1">
+                <a href="https://accksa.com/service/equipment-rental/">
+                    <div class="menu-item-wrap">
+                        <span class="menu-item-title">Equipment
+                            Rental</span>
+                    </div>
+                </a>
+            </li>
+
+            <li id="menu-item-3584"
+                class="menu-item menu-item-type-post_type menu-item-object-service menu-item-3584 level-1">
+                <a href="https://accksa.com/service/fabrication-service">
+                    <div class="menu-item-wrap">
+                        <span class="menu-item-title">Fabrication
+                            Services</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <?php
+}
+
+function footer_mobile_nav($from_services){
+	?>
+<div id="page-mobile-main-menu" class="page-mobile-main-menu">
+		<div class="page-mobile-menu-header">
+			<div class="page-mobile-menu-logo">
+				<a href="https://accksa.com/" rel="home">
+					<img src="<?php echo (!$from_services) ? '../wp-content/uploads/new_pics/Logo.png' : '../../wp-content/uploads/new_pics/Logo.png'; ?>" alt="Logo" />
+				</a>
+			</div>
+			<div id="page-close-mobile-menu" class="page-close-mobile-menu">
+				<span class="fal fa-times"></span>
+			</div>
+		</div>
+
+		<div class="page-mobile-menu-content">
+			<ul id="mobile-menu-primary" class="menu__container">
+				<li
+					class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2419 current_page_item menu-item-2734 level-1">
+					<a href="https://accksa.com/">
+						<div class="menu-item-wrap"><span class="menu-item-title">Home</span></div>
+					</a></li>
+				<li
+					class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-3758 level-1">
+					<a href="#">
+						<div class="menu-item-wrap"><span class="menu-item-title">About us</span><span
+								class="toggle-sub-menu"> </span></div>
+					</a>
+					<ul class="sub-menu">
+						<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3642"><a
+								href="https://accksa.com/about-us">
+								<div class="menu-item-wrap"><span class="menu-item-title">Who we are</span></div>
+							</a></li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3759"><a
+								href="https://accksa.com/md-message">
+								<div class="menu-item-wrap"><span class="menu-item-title">MD Message</span></div>
+							</a></li>
+					</ul>
+				</li>
+				<li
+					class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3493 level-1">
+					<a href="#">
+						<div class="menu-item-wrap"><span class="menu-item-title">Service</span><span
+								class="toggle-sub-menu"> </span></div>
+					</a>
+					<ul class="sub-menu">
+						<li class="menu-item menu-item-type-post_type menu-item-object-service menu-item-3494"><a
+								href="https://accksa.com/service/manpower-supply/">
+								<div class="menu-item-wrap"><span class="menu-item-title">Manpower Supply</span>
+								</div>
+							</a></li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-service menu-item-3760"><a
+								href="https://accksa.com/service/material-supply">
+								<div class="menu-item-wrap"><span class="menu-item-title">Material Supply</span>
+								</div>
+							</a></li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-service menu-item-3495"><a
+								href="https://accksa.com/service/equipment-rental/">
+								<div class="menu-item-wrap"><span class="menu-item-title">Equipment rental</span></div>
+							</a></li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-service menu-item-3581"><a
+								href="https://accksa.com/service/fabrication-service">
+								<div class="menu-item-wrap"><span class="menu-item-title">Fabrication Services</span></div>
+							</a></li>
+						
+						
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+<?php
+}
+
+function footer_address()
+{
+    ?>
+    <div class="tm-list  style-icon-02 tm-animation move-up" id="tm-list-6666aa6dd5306" style = "justify-items:center">
+        <div class="list-item">
+
+            <div class="list-header">
+                <div class="marker">
+
+                    <i class="tm-list__icon fa fa-envelope link-icon"></i>
+
+                </div>
+
+                <div class="title-wrap">
+                    <h6 class="title">
+					&nbsp;  info@accksa.com
+
+                    </h6>
+
+                </div>
+            </div>
+
+
+        </div>
+        <div class="list-item">
+
+            <div class="list-header">
+                <div class="marker">
+
+                    <i class="tm-list__icon fa fa-phone link-icon"></i>
+
+                </div>
+
+                <div class="title-wrap">
+                    <h6 class="title">
+					&nbsp;   +966 56 611 0003
+
+                    </h6>
+
+                </div>
+            </div>
+
+
+        </div>
+        <div class="list-item">
+
+            <div class="list-header">
+                <div class="marker">
+
+                    <i class="tm-list__icon fa fa-map-marker-alt link-icon"></i>
+
+                </div>
+
+                <div class="title-wrap">
+                    <h6 class="title">
+                        Mecca Street, Al
+                        Safat, Al Jubail 35514
+                        Kingdom of Saudi Arabia
+                    </h6>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <?php
+}
